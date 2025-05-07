074c1c598a5bc434f0fcd2c428b429581cfd8730
Arthur <arthurquintino2304@gmail.com>
2025-05-07 07:43:50 -0300

    Criando o script do banco

---
+++ script.sql
@@ -0,0 +1,19 @@
+-- CRIANDO BANCO DE DADOS 
+
+create database bdExemplo;
+
+--  USANDO O BANCO DE DADOS
+
+use bdExemplo;
+
+-- CRIANDO TABELAS DO BANCO
+
+create table Usuarios(
+	Id int primary key auto_increment,
+    Nome varchar(50) not null,
+    Email varchar(50) not null, 
+    Senha varchar(50) not null
+)
+-- CONSULTANDO OS DADOS DO BANCO
+
+select * from Usuarios;
